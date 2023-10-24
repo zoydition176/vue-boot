2cf852914e6bdbfdf6d5284f28894960364f9c7d
zdx <175851129@qq.com>
2023-10-24 15:15:08 +0800

    router

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue';
 import './style.css';
 import pinia from "/@/stores";
+import router from "/@/router";
 import App from './App.vue';
 
 const app = createApp(App);
-app.use(pinia).mount('#app');
+app.use(pinia).use(router).mount('#app');
 

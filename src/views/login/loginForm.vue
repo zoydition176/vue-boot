6d399055278f99f138d1cad4bab9465a894ed4de
zdx <175851129@qq.com>
2024-01-03 17:58:33 +0800

    1

--- src/views/login/loginForm.vue
+++ src/views/login/loginForm.vue
@@ -1,24 +1,42 @@
 <template>
   <div class="login-form">
-    23123login
-    {{ formState }}
-    <el-button @click="login">login</el-button>
+    <el-form ref="loginFormRef">
+      <el-form-item prop="username" label="用户名">
+        <el-input v-model="formState.username" />
+      </el-form-item>
+      <el-form-item prop="password" label="口令">
+        <el-input v-model="formState.password" />
+      </el-form-item>
+    </el-form>
+    <el-button :icon="CircleClose" round size="large" @click="resetForm()"> 重置 </el-button>
+    <el-button :icon="UserFilled" round size="large" type="primary" :loading="loading" @click="login(loginFormRef)">
+      登录
+    </el-button>
   </div>
 </template>
 <script setup lang="ts" name="loginForm">
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
+import { CircleClose, UserFilled } from "@element-plus/icons-vue";
+import type { FormInstance } from "element-plus";
 interface FormState {
   username: string;
   password: string;
   remember: boolean;
 }
+const loginFormRef = ref<FormInstance>();
+const loading = ref(false);
 const formState = reactive<FormState>({
   username: '',
   password: '',
   remember: true,
 });
+
 function login() {
-  formState.username =
+  console.log(formState); 
+}
+
+function resetForm() {
+
 }
 
 </script>

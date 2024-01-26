bda686fb7359aa49903996d3bf5b13c33eccaa6a
zdx <175851129@qq.com>
2024-01-26 18:04:40 +0800

    tabs

---
+++ src/layouts/components/tags/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="tags">
+
+  </div>
+</template>
+<script setup lang="ts" name="tags">
+
+</script>
+<style scoped lang="scss">
+
+</style>
---
+++ src/stores/modules/tabs.ts
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+
+export const tabStore = defineStore('tabs',{
+  state: () => ({
+    tabsList: []
+  }),
+  actions: {
+    addTabsList(){
+
+    }
+  },
+})

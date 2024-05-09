4ea9fb72837a4d68f1b49aacb7e420efbc07b332
zoydition176 <zdx175851129@gmail.com>
2024-05-09 09:08:57 +0800

    fix: test

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
-    <div>home view</div>
+    <div>home view11</div>
   </div>
 </template>
-<script setup lang="ts" name="home">
-
-</script>
-<style scoped>
-
-</style>
+<script setup lang="ts" name="home"></script>
+<style scoped></style>
